c2567196f746939c173288f8c6e413b567800251
Lucas Ciziks <luciziks@hotmail.com>
2023-08-24 19:31:38 -0300

    Organizando crawler dos dados meteorológicos

---
+++ data_open_meteo/get_climate_data.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+import requests
+
+# !https://open-meteo.com/en/docs/historical-weather-api
+# URL Base da API Open-Meteo
+url = "https://archive-api.open-meteo.com/v1/archive"
+
+# Variáveis metereológicas de interesse ao problema
+climate_variables = [
+    "rain",
+    "temperature_2m",
+    "apparent_temperature",
+    "relativehumidity_2m",
+    "cloudcover",
+    "windspeed_10m",
+    "is_day",
+]
+
+# Parâmetros da requisição
+params = {
+    "latitude": "-7.43280012",
+    "longitude": "-40.68261908",
+    "start_date": "2022-01-01",
+    "end_date": "2022-01-01",
+    "timezone": "America/Sao_Paulo",
+    "hourly": ",".join(climate_variables),
+}
+
+# Requisitando informações com método GET
+response = requests.get(url, params)
+
+# Printando resposta da API
+response_json = response.json()
+pprint(response_json)
